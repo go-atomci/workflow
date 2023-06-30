b401343c475f8196a9a8579b668e1393dad97b1a
colynn.liu <colynn.liu@gmail.com>
2023-06-30 11:33:43 +0800

    🐞 fix: add missing quote

--- jenkins/templates/cipipeline.go
+++ jenkins/templates/cipipeline.go
@@ -21,7 +21,7 @@ const CIPipeline = `
 pipeline {
     agent {
         kubernetes {
-            defaultContainer {{ .DefaultContainer }}
+            defaultContainer "{{ .DefaultContainer }}"
             yaml """
 apiVersion: v1
 kind: Pod
